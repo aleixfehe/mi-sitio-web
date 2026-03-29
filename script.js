211975e2d3c8ce3b477bc82a5bed3956e26d7eac
aleixfehe <145052210+aleixfehe@users.noreply.github.com>
2026-03-29 20:57:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const toggle = document.querySelector('.menu-toggle');
 const links = document.querySelector('.nav-links');
 
+
 if (toggle && links) {
   toggle.addEventListener('click', () => {
     links.classList.toggle('open');
   });
 }
+
+
